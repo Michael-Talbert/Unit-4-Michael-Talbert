3f44c7277d92f6b0583944f9ab5d05fb2a92e5c0
Michael Talbert <michael-talbert>
2019-11-01 09:24:35 -0700

    updated car classes

--- Car.java
+++ Car.java
@@ -2,12 +2,12 @@ public class Car{
 
 private String make;
 private String model;
-private int fuelEfficency; //MPG
+private double fuelEfficency; //MPG
 private double tankSize;  //In Gallons
 private double amtFuel;  //In gallons
 
 
-public Car(String carMake, String carModel, double carMPG, double tankSize){
+public Car(String carMake, String carModel, double carMPG, double carTank){
   make = carMake;
   model = carModel;
   fuelEfficency = carMPG;
@@ -19,14 +19,15 @@ public void drive(double milesDriven){
  double fuelUsed = milesDriven/fuelEfficency;
  if (fuelUsed > amtFuel){
    double milesTraveled = amtFuel*fuelEfficency;
-   System.out.println("you ran out of gas and only got "+milesTraveled+ "miles")
+   System.out.println("you ran out of gas and only got "+milesTraveled+ "miles");
  } else {
  amtFuel -= fuelUsed;
 }
+}
 
-public addFuel(double fuelAdded){
+public void addFuel(double fuelAdded){
   if (fuelAdded > (tankSize-amtFuel)){
-    System.out.println("You added too much fuel")
+    System.out.println("You added too much fuel");
   }else {
     amtFuel += fuelAdded;
   }
@@ -34,8 +35,8 @@ public addFuel(double fuelAdded){
 
 public String toString(){
   String result = "";
-  result += "Make:"+make+"\nModel: "+model+"\nMPG: "+fuelEfficency;
-  result += "\nTank Size: "+tankSize+"\nCurrent Ammount of fuel: "+amtFuel;
+  result += "\n \n Make:"+make+"\nModel: "+model+"\nMPG: "+fuelEfficency;
+  result += "\nTank Size: "+tankSize+"\nCurrent Ammount of fuel: "+amtFuel+"\n \n";
   return result;
 }
 
--- CarTester.java
+++ CarTester.java
@@ -3,13 +3,15 @@ public class CarTester{
   public static void main(String[] args){
  Car myCar = new Car("Toyota", "sUpRA", 15.0, 23.5);
 
- System.out.Println(myCar.toString());
+ System.out.println(myCar.toString());
 
  myCar.drive(35.4); //drives the car for 35.4 miles
 
- System.out.println("Drove 35.4 miles "+myCar);
+ System.out.println("Drove 35.4 miles \n"+myCar);
 
  myCar.addFuel(1.0);
+System.out.println("You added 1 gallon of gas");
+
 
  System.out.println(myCar);
   }
